f5cc9a4b44a68d1d55e98222b0761ec7475e88eb
Emo <emotupev11@gmail.com>
2023-01-28 00:02:32 +0200

    feat: add new dataset info for products

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,10 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+document.querySelectorAll(".product").forEach(p => {
+    let price = p.querySelector(".price").innerHTML;
+    p.dataset.price = price;
+  })
 
 
 });
